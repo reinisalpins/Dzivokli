bee38530bddf3364abe8bfb088757054f9ccd6bb
reinisalpins <reinisalpins@gmail.com>
2022-04-13 02:05:11 +0300

    update

---
+++ admin/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <title>Admin panelis</title>
+</head>
+<body>
+
+    <section class="navigacija">
+    
+    <div class="sidebar">
+    <i class="fas fa-user"></i>
+    <p>Sveicināti: Reinis</p>
+    <hr>
+  <a href="#home">Sākumlapa</a>
+  <a href="#news">Sludinājumi</a>
+  <a href="#contact">Lietotāji</a>
+  <a href="#about">Lapas administrēšana</a>
+
+
+  <div class="logout">
+      <i class="fas fa-sign-out-alt"></i>
+  </div>
+
+  
+</div>
+    </section>
+
+</body>
+</html>
