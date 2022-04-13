b91864dee7781a7955a4707da8a075b33b576b2e
reinisalpins <reinisalpins@gmail.com>
2022-04-13 13:50:14 +0300

    update admin

--- admin/index.php
+++ admin/index.php
@@ -4,32 +4,27 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="styleLogin.css">
     <title>Admin panelis</title>
 </head>
 <body>
+<div class="container" id="container">
 
-    <section class="navigacija">
-    
-    <div class="sidebar">
-    <i class="fas fa-user"></i>
-    <p>Sveicināti: Reinis</p>
-    <hr>
-  <a href="#home" class="active">Sākumlapa</a>
-  <a href="#news">Sludinājumi</a>
-  <a href="#news">Rezervācijas</a>
-  <a href="#contact">Lietotāji</a>
-
-
-  <div class="logout">
-      <i class="fas fa-sign-out-alt"></i>
-      
-  </div>
-
-  
+	<div class="forma ielogoties">
+        <!-- <div class="info">
+        
+        </div> -->
+		<form action="" method="POST">
+            <img src="images/house.png" alt="">
+			<h1>Ielogoties sistēmā</h1>
+			
+			<input type="text" name="lietotajs" placeholder="Lietotājvārds"  required/>
+			<input type="password" name="parole" placeholder="Parole" required/>
+			<input type="submit" name="submit" class="poga" value="Ielogoties"/>
+            
+		</form>
+	</div>        
 </div>
-    </section>
-
+	
 </body>
 </html>
